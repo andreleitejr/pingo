7529b3ef2e9bee54075bd8468dab20656e4dbf97
andrejvnior <andre.lljr@live.com>
2022-10-06 13:51:44 -0300

    place read page redesign

--- lib/features/place/pages/read/place_read_page.dart
+++ lib/features/place/pages/read/place_read_page.dart
@@ -56,202 +56,169 @@ class _PlaceReadPageState extends State<PlaceReadPage>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            const SliverAppBar(
-              pinned: true,
-              backgroundColor: Colors.white,
-              automaticallyImplyLeading: false,
-              elevation: 0,
-              title: DesignAppBar(
-                actionIcon: Icons.share,
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const DesignSpace(
-                        orientation: DesignSpaceOrientation.horizontal,
-                      ),
-                      DesignAvatarImage(image: widget.place.image),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.place.name,
-                              style: DesignTextStyle.bodyLarge18Bold,
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  widget.place.distance.metricSystem,
-                                  style: DesignTextStyle.bodySmall14,
-                                ),
-                                const SizedBox(width: 4),
-                                Container(
-                                  height: 12,
-                                  width: 1.25,
-                                  color: DesignColor.text300,
-                                ),
-                                const SizedBox(width: 4),
-                                const Icon(
-                                  Icons.heart_broken,
-                                  size: 14,
-                                  color: DesignColor.primary500,
-                                ),
-                                const SizedBox(width: 4),
-                                Text(
-                                  '${widget.place.rating.toString()} (${widget.place.ratings.length})',
-                                  style: DesignTextStyle.bodySmall14,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      const DesignSpace(
-                        orientation: DesignSpaceOrientation.horizontal,
-                      ),
-                    ],
-                  ),
-                  const DesignSpace(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: DesignSize.mediumSpace),
-                    child: Text(
-                      widget.place.description ??
-                          'No description found for this place',
-                      style: DesignTextStyle.bodySmall14,
-                    ),
-                  ),
-                  const DesignSpace(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: DesignSize.mediumSpace),
-                    child: Text(
-                      '${widget.place.address.line ?? 'Address not found'}, ${widget.place.address.number}',
-                      style: DesignTextStyle.bodySmall14
-                          .apply(color: DesignColor.primary700),
-                    ),
-                  ),
-                  const DesignSpace(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: DesignSize.mediumSpace),
-                    child: DesignButton(
-                      onPressed: () =>
-                          Get.to(RatingPage(ratedId: widget.place.uuid)),
-                      title: 'Rate',
-                      isActive: true,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SliverToBoxAdapter(child: const DesignSpace()),
-            SliverAppBar(
-              pinned: true,
-              backgroundColor: Colors.white,
-              automaticallyImplyLeading: false,
-              elevation: 1,
-              toolbarHeight: 8,
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(40.0),
-                child: TabBar(
-                  padding: EdgeInsets.zero,
-                  tabs: tabs,
-                  controller: tabController,
-                  unselectedLabelColor: DesignColor.text400,
-                  indicatorColor: DesignColor.primary500,
-                  labelColor: DesignColor.primary500,
+        child: Obx(
+          () => CustomScrollView(
+            slivers: <Widget>[
+              const SliverAppBar(
+                pinned: true,
+                backgroundColor: Colors.white,
+                automaticallyImplyLeading: false,
+                elevation: 0,
+                title: DesignAppBar(
+                  actionIcon: Icons.share,
                 ),
               ),
-            ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: _height,
-                child: TabBarView(
-                  controller: tabController,
+              SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // Wrap(
-                    //   children: [
-                    //     for(var i = 0; i <= 100; i++)...[
-                    //       Container(
-                    //         alignment: Alignment.center,
-                    //         height: (Get.width - 2) / 3,
-                    //         width: (Get.width - 2) / 3,
-                    //         child: Text(i.toString()),
-                    //       )
-                    //     ]
-                    //   ],
-                    // ),
-                    GridView.count(
-                      physics: NeverScrollableScrollPhysics(),
-                      // to disable GridView's scrolling
-                      shrinkWrap: true,
-                      // Create a grid with 2 columns. If you change the scrollDirection to
-                      // horizontal, this produces 2 rows.
-                      mainAxisSpacing: 2,
-                      crossAxisSpacing: 2,
-                      crossAxisCount: 3,
-                      // Generate 100 widgets that display their index in the List.
-                      children: List.generate(_listHeight, (index) {
-                        print('salsahiosahsoihsoihsai $isDivisible');
-                        return Container(
-                          alignment: Alignment.center,
-                          color: Colors.grey,
-                          child: Text((index + 1).toString()),
-                        );
-                      }),
+                    Row(
+                      children: [
+                        const DesignSpace(
+                          orientation: DesignSpaceOrientation.horizontal,
+                        ),
+                        DesignAvatarImage(image: widget.place.image),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.place.name,
+                                style: DesignTextStyle.bodyLarge18Bold,
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    widget.place.distance.metricSystem,
+                                    style: DesignTextStyle.bodySmall14,
+                                  ),
+                                  const SizedBox(width: 4),
+                                  Container(
+                                    height: 12,
+                                    width: 1.25,
+                                    color: DesignColor.text300,
+                                  ),
+                                  const SizedBox(width: 4),
+                                  const Icon(
+                                    Icons.heart_broken,
+                                    size: 14,
+                                    color: DesignColor.primary500,
+                                  ),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                    '${widget.place.rating.toString()} (${widget.place.ratings.length})',
+                                    style: DesignTextStyle.bodySmall14,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        const DesignSpace(
+                          orientation: DesignSpaceOrientation.horizontal,
+                        ),
+                      ],
                     ),
-                    // SliverGrid(
-                    //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    //       crossAxisCount: 3,
-                    //       mainAxisSpacing: 2,
-                    //       crossAxisSpacing: 2,
-                    //       childAspectRatio: 1),
-                    //   delegate: SliverChildBuilderDelegate(
-                    //         (BuildContext context, int index) {
-                    //       return Obx(() {
-                    //         if (controller.currentTab.value ==
-                    //             PlaceTabItemValue.photos) {
-                    //           return Container(
-                    //             padding: EdgeInsets.all(8),
-                    //             color: Colors.grey,
-                    //             child: Center(
-                    //               child: Text(
-                    //                 index.toString(),
-                    //                 textAlign: TextAlign.center,
-                    //                 style: TextStyle(color: Colors.white, fontSize: 20),
-                    //               ),
-                    //             ),
-                    //           );
-                    //         }
-                    //         return Container();
-                    //       });
-                    //     },
-                    //     childCount: 100,
-                    //   ),
-                    // ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.pink,
-                      child: Text('MAP'),
+                    const DesignSpace(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: DesignSize.mediumSpace),
+                      child: Text(
+                        widget.place.description ??
+                            'No description found for this place',
+                        style: DesignTextStyle.bodySmall14,
+                      ),
                     ),
-                    Container(
-                      child: Text('RATINGS'),
+                    const DesignSpace(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: DesignSize.mediumSpace),
+                      child: Text(
+                        '${widget.place.address.line ?? 'Address not found'}, ${widget.place.address.number}',
+                        style: DesignTextStyle.bodySmall14
+                            .apply(color: DesignColor.primary700),
+                      ),
+                    ),
+                    const DesignSpace(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: DesignSize.mediumSpace),
+                      child: DesignButton(
+                        onPressed: () =>
+                            Get.to(RatingPage(ratedId: widget.place.uuid)),
+                        title: 'Rate',
+                        isActive: true,
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+              SliverToBoxAdapter(child: const DesignSpace()),
+              SliverAppBar(
+                pinned: true,
+                backgroundColor: Colors.white,
+                automaticallyImplyLeading: false,
+                elevation: 1,
+                titleSpacing: 0,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    for (final tab in tabs) ...[tab],
+                  ],
+                ),
+              ),
+              if (controller.currentTab.value == PlaceTabItemValue.photos) ...[
+                SliverGrid(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 2,
+                      crossAxisSpacing: 2,
+                      childAspectRatio: 1),
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      if (controller.currentTab.value ==
+                          PlaceTabItemValue.photos) {
+                        return Container(
+                          padding: const EdgeInsets.all(8),
+                          color: DesignColor.text200,
+                          child: Center(
+                            child: Text(
+                              index.toString(),
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: DesignColor.text300,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                      return Container();
+                    },
+                    childCount: 100,
+                  ),
+                ),
+              ] else if (controller.currentTab.value ==
+                  PlaceTabItemValue.map) ...[
+                SliverFillRemaining(
+                  child: Container(
+                    color: DesignColor.text200,
+                    alignment: Alignment.center,
+                    child: Text('Map'),
+                  ),
+                ),
+              ] else ...[
+                SliverFillRemaining(
+                  child: Container(
+                    color: DesignColor.text200,
+                    alignment: Alignment.center,
+                    child: Text('Ratings'),
+                  ),
+                ),
+              ],
+            ],
+          ),
         ),
       ),
     );
@@ -280,12 +247,32 @@ class PlaceTabItem extends StatelessWidget {
   final PlaceTabItemValue value;
   final IconData iconData;
 
+  final PlaceReadController controller = Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return Tab(
-      icon: Icon(
-        iconData,
-      ),
+    return GestureDetector(
+      onTap: () => controller.setTabItem(value),
+      child: Obx(() {
+        final isSelected = controller.currentTab.value == value;
+
+        return Container(
+          width: (Get.width / 3) - 2,
+          height: 56,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: isSelected ? DesignColor.primary500 : Colors.transparent,
+                width: 2,
+              ),
+            ),
+          ),
+          child: Icon(
+            iconData,
+            color: isSelected ? DesignColor.primary500 : DesignColor.text400,
+          ),
+        );
+      }),
     );
   }
 }
